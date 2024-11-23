790542222b60cd0a6f9eba6440b471ca9a1cd280
embedded-kiddie <kings.unlikely@gmail.com>
2024-11-23 10:57:48 +0900

    Merge printf.hpp from Widget.

--- MLX90640/printf.hpp
+++ MLX90640/printf.hpp
@@ -26,15 +26,6 @@ int printf(const char* fmt, ...) {
 #endif
 
 #ifdef  GFX_EXEC
-/*--------------------------------------------------------------------------------
- * Definitions of graphics helpers
- *--------------------------------------------------------------------------------*/
-// Font size for setTextSize(2)
-#ifndef FONT_WIDTH
-#define FONT_WIDTH    12 // [px] (Device coordinate system)
-#define FONT_HEIGHT   16 // [px] (Device coordinate system)
-#define LINE_HEIGHT   18 // [px] (FONT_HEIGHT + margin)
-#endif
 
 void gfx_printf(uint16_t x, uint16_t y, const char* fmt, ...) {
   int len = 0;
@@ -45,13 +36,8 @@ void gfx_printf(uint16_t x, uint16_t y, const char* fmt, ...) {
   len = vsnprintf(buf, sizeof(buf), fmt, arg_ptr);
   va_end(arg_ptr);
 
-#if 0
-  GFX_EXEC(fillRect(x, y, len * FONT_WIDTH, FONT_HEIGHT, BLACK));
-#else
-//GFX_EXEC(setTextColor(WHITE, BLACK)); // Use opaque text output
-#endif
-  GFX_EXEC(setCursor(x, y));
-  GFX_EXEC(print(buf));
+  // use setTextColor(foreground_color, background_color) and setTextDatum(textdatum_t::...)
+  GFX_EXEC(drawString(buf, x, y));
 }
 
 #endif
